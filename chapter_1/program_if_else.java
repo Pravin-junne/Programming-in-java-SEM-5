0551c20eaf6fd398c8046b0218a59c7d743e14e0
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-09-06 22:41:28 +0530

    if_else_program-01

---
+++ chapter_1/program_if_else.java
@@ -0,0 +1,13 @@
+//if_else program to check voting criteria..
+public class program_if_else {
+    public static void main(String args[]){
+        int age = 20;
+        
+        if(age>=18){
+            System.out.print("You can vote...");
+        }
+        else{
+            System.out.print("you can not vote...");
+        }
+    }    
+}
